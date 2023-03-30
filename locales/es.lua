e48821deb2a134f9fe5c3f44a05268298bdd121e
pk <123796031+pelaoloko@users.noreply.github.com>
2023-03-30 18:50:11 -0300

    Update es.lua

--- locales/es.lua
+++ locales/es.lua
@@ -1,9 +1,9 @@
 Locales['es'] = {
-  ['valid_purchase'] = '¿validar esta compra?',
-  ['yes'] = 'si',
-  ['no'] = 'no',
-  ['not_enough_money'] = 'no tienes suficente dinero',
-  ['press_access'] = 'presiona [E] para acceder al menú',
-  ['barber_blip'] = 'barbero',
-  ['you_paid'] = 'has pagado $%s',
+  ['valid_purchase'] = '¿Desea realizar esta compra?',
+  ['yes'] = 'Si',
+  ['no'] = 'No',
+  ['not_enough_money'] = 'No tienes suficente dinero',
+  ['press_access'] = 'Presiona [E] para acceder a la barberia',
+  ['barber_blip'] = 'Barbero',
+  ['you_paid'] = 'Has pagado $%s',
 }
