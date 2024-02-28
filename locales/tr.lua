b7625393d52e377a6fc7edea6df44bcb1123f1f8
TitanHaxz <titanhaxz@gmail.com>
2024-02-28 23:40:03 +0300

    Create tr.lua

---
+++ locales/tr.lua
@@ -0,0 +1,9 @@
+Locales['tr'] = {
+  ['valid_purchase'] = 'Değişikliği onaylıyor musun?',
+  ['yes'] = 'Evet',
+  ['no'] = 'Hayır',
+  ['not_enough_money'] = 'Yeterli paran yok',
+  ['press_access'] = 'Kuaför Salonuna girmek için ~b~[E]~s~ tuşuna bas.',
+  ['barber_blip'] = 'Kuaför Salonu',
+  ['you_paid'] = 'Ödeme yaptınız: $%s',
+}
